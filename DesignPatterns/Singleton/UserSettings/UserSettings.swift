8a18e4bc0529e991271313412b8340d9b36c27b9
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-17 14:42:40 -0600

    Reads/Write lock technique implemented

--- DesignPatterns/Singleton/UserSettings/UserSettings.swift
+++ DesignPatterns/Singleton/UserSettings/UserSettings.swift
@@ -10,13 +10,13 @@ import Foundation
 final class UserSettings {
     public static let shared: UserSettings = UserSettings()
     private var settings: [String: Any] = ["Theme": "Vivid", "Brightness": 50]
-    private let serialQueue: DispatchQueue = DispatchQueue(label: "serialQueue")
+    private let concurrentQueue: DispatchQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
     
     private init() { }
     
     public func string(forKey key: String) -> String? {
         var result: String?
-        serialQueue.sync { [weak self] in
+        concurrentQueue.sync { [weak self] in
             guard let self = self else { return }
             result = self.settings[key] as? String
         }
@@ -25,7 +25,7 @@ final class UserSettings {
     
     public func int(forKey key: String) -> Int? {
         var result: Int?
-        serialQueue.sync { [weak self] in
+        concurrentQueue.sync { [weak self] in
             guard let self = self else { return }
             result = self.settings[key] as? Int
         }
@@ -33,7 +33,7 @@ final class UserSettings {
     }
     
     public func set(value: Any, forKey key: String) {
-        serialQueue.sync { [weak self] in
+        concurrentQueue.async(flags: .barrier) { [weak self] in
             guard let self = self else { return }
             self.settings[key] = value
         }
