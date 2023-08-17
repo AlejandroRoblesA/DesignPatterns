d00c54beeb1ebdddd4db61eb48f582d49d7897f1
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-17 14:23:17 -0600

    Singleton thread-safe

--- DesignPatterns/Singleton/UserSettings/UserSettings.swift
+++ DesignPatterns/Singleton/UserSettings/UserSettings.swift
@@ -8,19 +8,34 @@
 import Foundation
 
 final class UserSettings {
-    public static let shared = UserSettings()
+    public static let shared: UserSettings = UserSettings()
     private var settings: [String: Any] = ["Theme": "Vivid", "Brightness": 50]
+    private let serialQueue: DispatchQueue = DispatchQueue(label: "serialQueue")
+    
     private init() { }
     
     public func string(forKey key: String) -> String? {
-        return settings[key] as? String
+        var result: String?
+        serialQueue.sync { [weak self] in
+            guard let self = self else { return }
+            result = self.settings[key] as? String
+        }
+        return result
     }
     
     public func int(forKey key: String) -> Int? {
-        return settings[key] as? Int
+        var result: Int?
+        serialQueue.sync { [weak self] in
+            guard let self = self else { return }
+            result = self.settings[key] as? Int
+        }
+        return result
     }
     
     public func set(value: Any, forKey key: String) {
-        settings[key] = value
+        serialQueue.sync { [weak self] in
+            guard let self = self else { return }
+            self.settings[key] = value
+        }
     }
 }
