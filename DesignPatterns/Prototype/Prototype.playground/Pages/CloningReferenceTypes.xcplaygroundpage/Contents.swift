9843d79d55d8ab1b1d2fcaa7fa7c7317be938b49
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-08-19 22:15:21 -0600

    Contact Class Model

--- DesignPatterns/Prototype/Prototype.playground/Pages/CloningReferenceTypes.xcplaygroundpage/Contents.swift
+++ DesignPatterns/Prototype/Prototype.playground/Pages/CloningReferenceTypes.xcplaygroundpage/Contents.swift
@@ -6,3 +6,12 @@ var greeting = "Hello, playground"
 
 //: [Next](@next)
 
+class Contact {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
